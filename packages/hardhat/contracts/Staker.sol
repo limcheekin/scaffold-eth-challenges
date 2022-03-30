4fd7f9deaea860b58d828b68ed44a1d62c7cd082
Lim Chee Kin <limcheekin@vobject.com>
2022-03-30 16:44:35 +0800

    feat: removed the redundant balance state variable, added modifiers and improved codes

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -7,19 +7,28 @@ import "./ExampleExternalContract.sol";
 contract Staker {
     ExampleExternalContract public exampleExternalContract;
     mapping(address => uint256) public balances;
-    uint256 public balance;
-    uint256 deadline;
-    uint256 public threshold = 1 ether;
+    uint256 deadline = block.timestamp + 30 seconds;
+    uint256 public constant threshold = 1 ether;
 
     event Stake(address staker, uint256 amount);
+
+    modifier isStakingCompleted() {
+        require(
+            !exampleExternalContract.completed(),
+            "Staking event is completed!"
+        );
+        _;
+    }
+
+    modifier isDeadlineOver() {
+        require(deadline < block.timestamp, "Deadline is not over!");
+        _;
+    }
 
     constructor(address exampleExternalContractAddress) {
         exampleExternalContract = ExampleExternalContract(
             exampleExternalContractAddress
         );
-
-        // REF: https://programtheblockchain.com/posts/2018/01/12/writing-a-contract-that-handles-time/
-        deadline = block.timestamp + 1 hours;
     }
 
     // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
@@ -27,7 +36,6 @@ contract Staker {
     function stake() public payable {
         uint256 value = msg.value;
         require(value > 0, "stake amount cannot be zero");
-        balance += value;
         address sender = msg.sender;
         balances[sender] = value;
         emit Stake(sender, value);
@@ -39,8 +47,7 @@ contract Staker {
 
     // After some `deadline` allow anyone to call an `execute()` function
     // It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-    function execute() external {
-        require(deadline < block.timestamp, "Deadline is not over!");
+    function execute() external isStakingCompleted {
         uint256 total = address(this).balance;
         if (total >= threshold) {
             exampleExternalContract.complete{value: total}();
@@ -50,11 +57,8 @@ contract Staker {
     // if the `threshold` was not met, allow everyone to call a `withdraw()` function
     // Add a `withdraw()` function to let users withdraw their balance
     // REF: https://docs.soliditylang.org/en/v0.8.7/common-patterns.html
-    function withdraw() external {
-        require(
-            !exampleExternalContract.completed(),
-            "Staking event is completed, no more withdrawal!"
-        );
+    function withdraw() external isDeadlineOver {
+        require(address(this).balance < threshold, 'Threshold met, no more withdrawal!');
         uint256 amount = balances[msg.sender];
         require(amount > 0, "No amount available to withdraw!");
         // Remember to zero the pending refund before
