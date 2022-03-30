1205bcc917504ba1d7b2b87119b3b1775392e009
Lim Chee Kin <limcheekin@vobject.com>
2022-03-30 16:15:23 +0800

    feat: first import of staking contract that passed all tests

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,35 +1,70 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    ExampleExternalContract public exampleExternalContract;
+    mapping(address => uint256) public balances;
+    uint256 public balance;
+    uint256 deadline;
+    uint256 public threshold = 1 ether;
 
-  ExampleExternalContract public exampleExternalContract;
+    event Stake(address staker, uint256 amount);
 
-  constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    constructor(address exampleExternalContractAddress) {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+        // REF: https://programtheblockchain.com/posts/2018/01/12/writing-a-contract-that-handles-time/
+        deadline = block.timestamp + 1 hours;
+    }
 
+    // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+    //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    function stake() public payable {
+        uint256 value = msg.value;
+        require(value > 0, "stake amount cannot be zero");
+        balance += value;
+        address sender = msg.sender;
+        balances[sender] = value;
+        emit Stake(sender, value);
+    }
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    function timeLeft() external view returns (uint256) {
+        return deadline > block.timestamp ? deadline - block.timestamp : 0;
+    }
 
+    // After some `deadline` allow anyone to call an `execute()` function
+    // It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    function execute() external {
+        require(deadline < block.timestamp, "Deadline is not over!");
+        uint256 total = address(this).balance;
+        if (total >= threshold) {
+            exampleExternalContract.complete{value: total}();
+        }
+    }
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    // Add a `withdraw()` function to let users withdraw their balance
+    // REF: https://docs.soliditylang.org/en/v0.8.7/common-patterns.html
+    function withdraw() external {
+        require(
+            !exampleExternalContract.completed(),
+            "Staking event is completed, no more withdrawal!"
+        );
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No amount available to withdraw!");
+        // Remember to zero the pending refund before
+        // sending to prevent re-entrancy attacks
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
 
-
-  // Add a `withdraw()` function to let users withdraw their balance
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
-
+    // Add the `receive()` special function that receives eth and calls stake()
+    receive() external payable {
+        stake();
+    }
 }
