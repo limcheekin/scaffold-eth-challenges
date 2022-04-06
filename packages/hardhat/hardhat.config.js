80f24a3f5524765d85aa1578c3f5ea2cb05d70b8
Lim Chee Kin <limcheekin@vobject.com>
2022-04-06 18:53:05 +0800

    chore: added caniuse-lite dependency

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -62,6 +62,7 @@ module.exports = {
     rinkeby: {
       url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
+      gasPrice: 310 * 1000000000,
       accounts: {
         mnemonic: mnemonic(),
       },
